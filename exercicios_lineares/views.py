90e9a008183ee847ca8ed816a6e14fcadb7a15f8
Fernando Joanis <86856812+Fernando-Joanis@users.noreply.github.com>
2022-07-15 16:52:45 -0300

    Update views.py
    Implementing FileResponse and new exercise generator

--- exercicios_lineares/views.py
+++ exercicios_lineares/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from django import FileResponse
 from random import randint
+from exercicios_lineares import LinearExercises
 
 
 class Exercicios:
@@ -22,5 +23,16 @@ class ExercicioAPIView(APIView):
     API Exercicios Matematicos
     """
 
-    def get(self, request):
-        return Response(Exercicios().gerador())
+    def post(self, request):
+        data = request.data
+        operador = data['operador']
+        ordem = data['ordem']
+        intervalo1 = data['intervalo1']
+        intervalo2 = data['intervalo2']
+        filename = LinearExercises(operador=operador,
+                                            gerador=gerador,
+                                            pdf=pdf,
+                                            ordem=ordem,
+                                            intervalo1=intervalo1,
+                                            intervalo2=intervalo2).questions()
+        return FileResponse(open(filename), 'rb')
